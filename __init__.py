44db5d145e7ce2dd57545e4794498017929aa607
Shane Stephens <62107197+HeavyHeathen@users.noreply.github.com>
2022-04-22 13:22:10 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,6 +9,19 @@ class Snoopy(MycroftSkill):
 
     @intent_file_handler('snoopy.intent')
     def handle_snoopy(self, message):
+        nmScan.scan('192.1.1.1', '21-443')
+        
+        for host in nmScan.all_hosts():
+            print('Host : %s (%s)' % (host, nmScan[host].hostname()))
+            print('State : %s' % nmScan[host].state())
+            for proto in nmScan[host].all_protocols():
+                print('----------')
+                print('Protocol : %s' % proto)
+                
+                lport = nmScan[host][proto].keys()
+                lport.sort()
+                for port in lport:
+                    print('port : %s\tstate : %s' % (port, nmScan[host][proto][port]['state'])
         self.speak_dialog('snoopy')
 
 
